dce2675ad0082192d44de9f8541730dff606c3d3
PhucKhang1310 <npkhang2005@gmail.com>
2025-09-16 16:18:18 +0700

    migrated MainContainer.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
-import MainContainer from './components/MainContainer.tsx';
+import styles from './styles/app.module.scss';
+import BodyWrapper from './components/BodyWrapper.tsx';
+import NameBar from './components/NameBar.tsx';
+import NavBar from './components/NavBar.tsx';
 
 function App() {
   return (
-    <>
-      <MainContainer />
-    </>
+    <div className={styles.mainContainer}>
+      <NavBar />
+      <BodyWrapper />
+      <NameBar />
+    </div>
   );
 }
 
